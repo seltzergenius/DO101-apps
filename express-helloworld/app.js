9d1a1932175f879259d57028a1c76c220ddc285c
Grieco <KGrieco@cogfin.com>
2020-04-27 13:00:26 -0400

    removed covid line

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World, COVID sucks!\n');
+  res.send('Hello World!\n');
 });
 
 app.listen(8080, function () {
